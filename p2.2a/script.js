90df73416b1209d2576e23b6791b2fb88e6112db
skayla <90851580+skayla-nelie@users.noreply.github.com>
2023-04-28 00:49:55 -0400

    spritesheet

--- p2.2a/script.js
+++ p2.2a/script.js
@@ -3,10 +3,13 @@ gsap.fromTo(
   { y: "0%", opacity: 1, duration: 0 },
   { y: "50%", opacity: 0, duration: 3, repeat: -1 }
 );
+const body = document.querySelector("body");
+let isScrolling;
 
 window.addEventListener("scroll", function () {
   window.clearTimeout(isScrolling);
   body.classList.add("is-scrolling");
+
   isScrolling = setTimeout(function () {
     console.log("Scrolling has stopped.");
     body.classList.remove("is-scrolling");
